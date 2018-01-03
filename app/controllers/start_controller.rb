95f24a911c1e5ef29a4278d6c9a20520279f9f89
anantdimri96 <anant.dimri96@gmail.com>
2018-01-03 17:53:29 +0530

    added checkbox for completing task

--- app/controllers/start_controller.rb
+++ app/controllers/start_controller.rb
@@ -2,15 +2,18 @@ class StartController < ApplicationController
 
   def index
     if (user_signed_in?)
-       @user=current_user
-      # ExampleMailer.sample_email(@user).deliver_now
-      @tasks = current_user.tasks
-      @task = Task.where(date: Time.now.strftime("%d/%m/%Y")).where(user_id: current_user).first
+       	@tasks = current_user.tasks
 
+        # @task = @tasks.find_by(date: Time.now.strftime("%d/%m/%Y"))
+
+      	@dates = @tasks.group(:date).map(&:date).uniq
+
+       	@tasks_perday = @tasks.where(date: Time.now.strftime("%d/%m/%Y"))
+
+
+      # end
      # @data = params[:radios1]
-  else
-    redirect_to new_user_session_path
   end
-  end
+end
 
 end
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,15 +1,25 @@
 class TasksController < ApplicationController
   def create
-    				@task = Task.new(params.require(:task).permit(:to_do)
-						.merge(:user_id => current_user.id,
-								:date => Time.now.strftime("%d/%m/%Y")))
-		if(@task.save)
 
-    	redirect_to root_path
-		else
-			render plain: "error"
-		end
-	end
+    params[:task][:to_do].each_line.each do |task|
+    			Task.create(to_do: task, user_id: current_user.id, date: Time.now.strftime("%d/%m/%Y"))
+    end
+    redirect_to root_path
+end
+
+  #     			@task = Task.new(params.require(:task).permit(:to_do)
+	# 					.merge(:user_id => current_user.id,
+	# 							:date => Time.now.strftime("%d/%m/%Y")))
+	# 	if(@task.save)
+  #
+  #
+  #
+  #
+  #   	redirect_to root_path
+	# 	else
+	# 		render plain: "error"
+	# 	end
+	# end
 
    def set_checkbox
        task = Task.find(params[:plan_id])
@@ -25,7 +35,7 @@ class TasksController < ApplicationController
 
 	def update
 		set_task
-		if(@task.update!(params.require(:task).permit(:completed)))
+    		if(@task.update!(params.require(:task).permit(:completed)))
 			redirect_to root_path
 		end
 	end
